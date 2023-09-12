f6a61a526c025052922c3f7942df91f23a507c0e
Tushar Malik <102008446+maliktushar@users.noreply.github.com>
2023-09-12 11:19:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -11,29 +11,12 @@ let products = [
     quantity: 10
 }
 ];
-let users = [
-    {
-    name: "John",
-    email: "john@mail.com",
-    phone: 1234567890
-},
-{
-    name: "Jane",
-    email: "jane@mail.com",
-    phone: 1234567890
-}
-];
 localStorage.setItem('products', JSON.stringify(products));
-localStorage.setItem('users', JSON.stringify(users));
+
 // Retrieving data from Local Storage
 localStorage.getItem('products');
-localStorage.getItem('users');
+
 
 let productData=JSON.parse(localStorage.getItem('products'));
-let userData=JSON.parse(localStorage.getItem('users'));
-console.log(productData);
 
-/*// Removing data from Local Storage
-localStorage.removeItem('products');
-// Clearing the entire Local Storage
-localStorage.clear();*/
+console.log(productData);
